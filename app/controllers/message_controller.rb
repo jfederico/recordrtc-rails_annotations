e5baeca96ca677e92657d67cfef375883f1bf449
Jacob Prud'homme <jacob.prudhomme@blindsidenetworks.com>
2017-06-26 14:56:52 -0400

    Updated README
    Found out why Canvas was not working with LTI 1.0 registrations via config URL.

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -96,7 +96,9 @@ class MessageController < ApplicationController
                         else
                           {url: recordrtc_launch_url}
                         end
+
     navigation_params[:text] = 'RecordRTC'
+    navigation_params[:windowTarget] = '_blank'
 
     tc.set_ext_param(CanvasExtensions::PLATFORM, placement_key, navigation_params)
   end
