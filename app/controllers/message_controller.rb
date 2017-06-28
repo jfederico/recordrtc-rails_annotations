8e726f5e48eba11a5d4cb6574c056a85d417d8f2
Jacob Prud'homme <jacob.prudhomme@blindsidenetworks.com>
2017-06-28 13:46:05 -0400

    Fixed "User cannot be blank" error
    session[:user_id] = person_sourcedid || user_id would assign a blank string to session[:user_id], because person_sourcedid existed, it's just that it was empty. This could not be used to create an account.

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -110,11 +110,11 @@ class MessageController < ApplicationController
     @header = SimpleOAuth::Header.new(:post, request.url, @message.post_params, consumer_key: @message.oauth_consumer_key, consumer_secret: ENV['LTI_SECRET'], callback: 'about:blank')
 
     if @message.lti_version == 'LTI-2p0'
-      session[:user_id] = @message.custom_person_sourcedid || @message.user_id
-      session[:full_name] = @message.custom_person_name_full || 'Student View'
+      session[:user_id] = @message.custom_person_sourcedid.blank? ? @message.user_id : @message.custom_person_sourcedid
+      session[:full_name] = @message.custom_person_name_full.blank? ? 'Student View' : @message.custom_person_name_full
     elsif @message.lti_version == 'LTI-1p0'
-      session[:user_id] = @message.lis_person_sourcedid || @message.user_id
-      session[:full_name] = @message.lis_person_name_full || 'Student View'
+      session[:user_id] = @message.lis_person_sourcedid.blank? ? @message.user_id : @message.lis_person_sourcedid
+      session[:full_name] = @message.lis_person_name_full.blank? ? 'Student View' : @message.lis_person_name_full
     end
 
     redirect_to record_index_path
